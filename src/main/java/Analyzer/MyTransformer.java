108d9fa06e9d560a5f03d733bd98144efcc2b207
Prakash-27 <prakashs23007@gmail.com>
2022-02-25 09:16:44 -0500

    Retrying Testcase for 2 to 3 times before Failing

---
+++ src/main/java/Analyzer/MyTransformer.java
@@ -0,0 +1,28 @@
+package Analyzer;
+
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.annotations.ITestAnnotation;
+
+public class MyTransformer implements IAnnotationTransformer {
+	
+	
+	public void transformer(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod ) 
+	{
+		
+		
+		annotation.setRetryAnalyzer(RetryAnalyzer.class);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
